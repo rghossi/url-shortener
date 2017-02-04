23f42153737e8b59add31b819b062a1c54ce4a1f
rghossi <rafael.ghossi@gmail.com>
2017-02-04 17:51:59 -0200

    added mongodb connection

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var UrlController = require('./urlController');
+var mongoose = require('mongoose');
+
+var MONGODB_URI = process.env.database || 'mongodb://localhost/urlshortener';
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function(){
+  console.log("Successfully connected to mongodb!")
+})
+
+mongoose.connect(MONGODB_URI);
 
 app.get('/', function (req, res) {
 	res.send("Hello");
